81fabd0cef59b2e50616203189e0495925186a40
Bhadrayu09 <127506313+Bhadrayu09@users.noreply.github.com>
2026-02-24 11:36:01 +0530

    app file changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/home/Home";
+import About from './pages/about/About';
+import School from './pages/schools/Schoolpage';
+import Collegeanduniversitypage from './pages/collegeuniversity/Collegeanduniversitypage';
+import StudentsAndParents from './pages/studentsandparents/StudentsAndParents';
+import Contact from './pages/contact/Contact';
 
 function App() {
-  
-
   return (
     <>
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+                   <Route path='about' element={<About/>} />
+                   <Route path='school' element={<School/>} />
+                   <Route path='College&University' element={<Collegeanduniversitypage/>} />
+                    <Route path='students&parents' element={<StudentsAndParents/>} />
+                    <Route path='contact' element={<Contact/>} />
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
