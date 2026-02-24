90f104dac46ba163da7e9123839a2e194737c8fa
Bhadrayu09 <127506313+Bhadrayu09@users.noreply.github.com>
2026-02-24 20:39:46 +0530

    capital name added to components in RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-import WhatsAppButton from '../components/WhatsAppButton'
+import Footer from '../Components/Footer'
+import Header from '../Components/Header'
+import WhatsAppButton from '../Components/WhatsAppButton'
 
 function RootLayout() {
   return (
